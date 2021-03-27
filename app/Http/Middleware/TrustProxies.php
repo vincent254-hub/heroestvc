56752b4dd0996273ad735d5f55afe2cc1562b50b
vincent254-hub <vincentkhamala9@yahoo.com>
2021-03-27 17:12:01 +0300

    kuhgj

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,23 +1,35 @@
 <?php
 
-namespace App\Http\Middleware;
+// namespace App\Http\Middleware;
 
+// use Illuminate\Http\Request;
+// use Fideloper\Proxy\TrustProxies as Middleware;
+
+
+// <?php
+namespace App\Http\Middleware;
 use Illuminate\Http\Request;
 use Fideloper\Proxy\TrustProxies as Middleware;
-
 class TrustProxies extends Middleware
 {
-    /**
-     * The trusted proxies for this application.
-     *
-     * @var array
-     */
-    protected $proxies;
-
-    /**
-     * The headers that should be used to detect proxies.
-     *
-     * @var int
-     */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $proxies = '*';
+    protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB;
 }
+
+
+// class TrustProxies extends Middleware
+// {
+//     /**
+//      * The trusted proxies for this application.
+//      *
+//      * @var array
+//      */
+//     protected $proxies;
+
+//     /**
+//      * The headers that should be used to detect proxies.
+//      *
+//      * @var int
+//      */
+//     protected $headers = Request::HEADER_X_FORWARDED_ALL;
+// }
